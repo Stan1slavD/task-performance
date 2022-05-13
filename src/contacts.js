4b0eaa669b3f0597bd0170f6f23294ab0f71dbb5
Stanislav <stasdamaratskiy@gmail.com>
2022-05-13 18:28:22 +0300

    task-perfomance

--- src/contacts.js
+++ src/contacts.js
@@ -12,15 +12,11 @@ function addContacts() {
   contacts.appendChild(fragment);
 }
 
+
+
 contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+const value = Math.floor(contacts.scrollTop / 18);
+stickyHeader.textContent = value;
 });
 
 addContacts();
